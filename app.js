5c56263394ed8e75ed8c1dbff50128941e96a80e
Anthony <jessica@Jessicas-MacBook-Pro.local>
2022-08-01 15:51:45 -0700

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // API calls
 const BASE_URL = 'http://localhost:3000';
 const postsPath = 'todos';
-const getPosts = () => {
+const getToDoLists = () => {
   const postsEndPoint = [BASE_URL, postsPath].join('/');
   return fetch(postsEndPoint).then((response) => {
     return response.json();
@@ -9,15 +9,62 @@ const getPosts = () => {
 };
 
 
+// CONST
+const DomSelectors = {
+  root:'todo_list',
+  taskElements: {
+    title: 'task_title',
+    update: 'task_update',
+    delete: 'task_delete'
+  }
+};
+
+
+// STATE
+class State {
+  constructor() {
+    this._task = [];
+  }
+
+  get tasks(){
+
+  } 
+
+  set tasks(newTasks) {
+    this._task = newTasks;
+    // 
+  }
+
+}
+
+// recording how many tasks on the page
+let state = new State();
+
+// Task
+const renderTask = (task,element) => {
+  // render sth 
+  const tmp = tasks.map((task) => generateTaskTmp(task, DomSelectors.taskElements))
+    .join('');
+};
+
+const generateTaskTmp = (task, {title, update, delete}) => {
+  return 
+   `<li>
+   <h1 style= "${completed == true ? "text-decoration: line-through" : ""}"  
+   name ="btn-title-${task.id}"> ${task.title}</h1>
+   <button class="btn_update" name="btn-update-${task.id}">Update</button>    
+   <button class="btn_delete" name="btn-delete-${task.id}">Delete</button>
+   </li> `;
+};
+
 
 // INIT
 const init = () => {
-    renderPostPage();
-  
-    getPosts().then((data) => {
-      state.posts = data;
-    });
-  };
+  getToDoList().then((data) => {
+  state.posts = data
+  });
+  renderTask();
+};
   
 init();
   
